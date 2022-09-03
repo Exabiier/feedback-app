81a78de850518440352c652570275cf4d91d16f5
Exabiier <exabiier.bars@gmail.com>
2022-09-02 23:50:46 -0500

    Learned React.createElement
    How the JSX allows one to use javascript to allow a user to use HTML Elements for coding.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import React from 'react'
+
+function App() {
+
+// return React.createElement('h1', {className: "nooooo"}, 'nooo')
+        return (
+
+        
+    <div className="container">
+        <h1>My App</h1>  
+
+    </div>    
+    )
+}
+
+export default App
