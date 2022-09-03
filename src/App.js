5e6540f35b59cd329d04e592326577b6cf2e8685
Exabiier <exabiier.bars@gmail.com>
2022-09-03 15:43:55 -0500

    Update App.js
    Learned how to use conditionals so we can show content on a webpage.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,44 @@
 
 function App() {
 
-// return React.createElement('h1', {className: "nooooo"}, 'nooo')
-        return (
+
+
+    
+//     const title = "Blog Post";
+//     const body = "This is my blog post";
+//      const comments = [   
+//         {id: 1, text: "Comment one"},
+//         {id: 2, text: "Comment two"},
+//         {id: 3, text: "Comment two"},
+//      ]
+
+//      const loading = false
+//      const showComments = false
+
+//      if(loading) return <h1>Loading....</h1>
+
+// // return React.createElement('h1', {className: "nooooo"}, 'nooo')
+// return (
 
         
-    <div className="container">
-        <h1>My App</h1>  
+//     <div className="container">
+//         <h1>{title}</h1> 
+//         <p>{body}</p> 
+       
+//     {showComments ? <div className="comments">
+//     <h3> Comments ({comments.length})</h3>
+//     <ul>
+//         {comments.map((comments, index) => (
+//             <li key={index}>{comments.text}</li>
+//         ))}
+//     </ul>
 
-    </div>    
-    )
+// </div> : null}
+
+
+
+//     </div>    
+//     )
 }
 
 export default App
